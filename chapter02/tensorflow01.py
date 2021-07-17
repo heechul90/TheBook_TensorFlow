f76b641b8034f34afb53700c5a882c7ce2ad135a
heechullee <heechul4296@gmail.com>
2021-07-17 12:40:43 +0900

    basic tensorflow

--- chapter02/tensorflow01.py
+++ chapter02/tensorflow01.py
@@ -10,4 +10,66 @@ sns.set(style='darkgrind')
 
 ### 데이터 호출
 cols = ['price', 'maint', 'doors', 'persons', 'lug_capacity', 'safety','output']
-cars = pd.read_csv('../chap2/data/car_evaluation.csv', names=cols, header=None)
+cars = pd.read_csv('./chapter02/data/car_evaluation.csv', names=cols, header=None)
+cars
+
+
+### 데이터셋 분포
+plot_size = plt.rcParams['figure.figsize']
+plot_size[0] = 8
+plot_size[1] = 6
+plt.rcParams['figure.figsize'] = plot_size
+cars.output.value_counts().plot(kind='pie',
+                                autopct='%0.05f%%',
+                                colors=['lightblue', 'lightgreen', 'orange', 'pink'],
+                                explode=(0.05,0.05,0.05,0.05)
+                                )
+
+
+### 원-핫 인코딩 적용
+price = pd.get_dummies(cars.price, prefix='price')
+maint = pd.get_dummies(cars.maint, prefix='maint')
+
+doors = pd.get_dummies(cars.doors, prefix='doors')
+persons = pd.get_dummies(cars.persons, prefix='persons')
+
+lug_capacity = pd.get_dummies(cars.lug_capacity, prefix='lug_capacity')
+safety = pd.get_dummies(cars.safety, prefix='safety')
+
+labels = pd.get_dummies(cars.output, prefix='condition')
+
+X = pd.concat([price, maint, doors, persons, lug_capacity, safety], axis=1)
+y = labels.values
+
+
+### 훈련과 검증 데이터셋으로 분리
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+
+
+### 모델생성 및 컴파일
+from tensorflow.keras.layers import Input, Dense, Activation, Dropout
+from tensorflow.keras.models import Model
+
+input_layer = Input(shape=(X.shape[1],))
+dense_layer_1 = Dense(15, activation='relu')(input_layer)
+dense_layer_2 = Dense(10, activation='relu')(dense_layer_1)
+output = Dense(y.shape[1], activation='softmax')(dense_layer_2)
+
+model = Model(inputs=input_layer, outputs=output)
+
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+
+model.summary()
+
+
+### 모델 훈련
+history = model.fit(X_train, y_train, batch_size=8, epochs=50, verbose=1, validation_split=0.2)
+
+
+### 모델검증
+score = model.evaluate(X_test, y_test, verbose=1)
+print("Test Score:", score[0])
+print("Test Accuracy:", score[1])
