9a6c354865c3b9601a03c3a7597151509c448d21
hclee <heechul4296@gmail.com>
2021-09-01 15:57:13 +0900

    update

--- chapter06/VGGNet.py
+++ chapter06/VGGNet.py
@@ -16,7 +16,6 @@ from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, Dropou
 class VGG19(Sequential):
     def __init__(self, input_shape):
         super().__init__()
-
         self.add(Conv2D(64, kernel_size=(3,3), padding='same',
                         activation='relu', input_shape=input_shape))
         self.add(Conv2D(64, kernel_size=(3,3), padding='same',
